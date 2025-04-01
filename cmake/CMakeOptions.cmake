cdc88b7bb1c00e13d94a1a9df9cbcf1397c0add7
Luong The Hai <hailuongthe2000@gmail.com>
2025-04-01 20:54:32 +0700

    Add docs

--- cmake/CMakeOptions.cmake
+++ cmake/CMakeOptions.cmake
@@ -1,2 +1,3 @@
 set(BUILD_EXAMPLES OFF CACHE BOOL "Enable to build examples")
 set(BUILD_TESTING OFF CACHE BOOL "Enable to build tests")
+set(BUILD_DOC OFF CACHE BOOL "Enable to build documentation")
